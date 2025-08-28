89018ffd3af62a868f0098895a4facc0c2599f00
kefeliz <felizk03@thryv.com>
2025-08-28 14:19:19 -0400

    Refactor useAuth hook to include pathname logic

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,17 +1,22 @@
 "use client";
 import { useUser } from "@auth0/nextjs-auth0";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export function useAuth(redirectToLogin: boolean = true) {
   const { user, error, isLoading } = useUser();
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     if (!isLoading && !user && redirectToLogin) {
       router.push("/authenticate");
     }
-  }, [isLoading, user, redirectToLogin, router]);
+
+    if (user && pathname === "/authenticate") {
+      router.push("/");
+    }
+  }, [isLoading, user, redirectToLogin, router, pathname]);
 
   return { user, error, isLoading };
 }
